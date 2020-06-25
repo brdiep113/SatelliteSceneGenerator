2469cb55663ccee6e0a1fb012d7ddbc70f900d0e
brian <brdiep113@gmail.com>
2020-06-24 22:48:19 -0400

    changes

--- GeneratorFunctions/plot_functions.py
+++ GeneratorFunctions/plot_functions.py
@@ -7,14 +7,18 @@ import random
 
 
 # constants
-my_dpi = 128
+my_dpi = 96
 SHAPE = (1024, 1024)
 
 
 def plot_sample_img(scene, image_outpath, image_count):
     noise = np.random.normal(255. / 2, 255. / 10, SHAPE)
 
-    img = plt.figure(figsize=(1024 / my_dpi, 1024 / my_dpi), dpi=my_dpi)
+    img = plt.figure()
+    DPI = img.get_dpi()
+    print(DPI)
+    img.set_size_inches(1330.0 / float(DPI), 1330.0 / float(DPI))
+
     axes = plt.gca()
     axes.imshow(noise, extent=[-1024, 1024, -1024, 1024])
     axes.set_xlim([0, 1024])
@@ -40,9 +44,25 @@ def plot_sample_img(scene, image_outpath, image_count):
         plt.grid(b=None)
         plt.box(False)
 
-    img.savefig(path.join(image_outpath, "image_{0}.png".format(image_count)), bbox_inches='tight', pad_inches=0)
-    plt.clf()
+    img.savefig(path.join(image_outpath, "image_{0}.png".format(image_count)), bbox_inches='tight', pad_inches=0, dpi=DPI)
+    plt.close()
 
+def plot_ground_truth(scene, image_outpath, image_count):
+    truth = plt.figure()
+    DPI = truth.get_dpi()
+    truth.set_size_inches(1330.0 / float(DPI), 1330.0 / float(DPI))
+
+    for structure in scene.buildings:
+        x, y = structure.matrix[:, 0], structure.matrix[:, 1]
+        plt.plot(x, y, 'o', color='black')
+
+    # Clear grid
+    plt.axis('off')
+    plt.grid(b=None)
+    plt.box(False)
+
+    truth.savefig(path.join(image_outpath, "image_{0}.png".format(image_count)), bbox_inches='tight', pad_inches=0, dpi=DPI)
+    plt.close()
 
 def plot_vectors(image, data_dictionary):
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from GeneratorFunctions.plot_functions import fill_scene, plot_sample_img, plot_vectors
+from GeneratorFunctions.plot_functions import fill_scene, plot_sample_img, plot_vectors, plot_ground_truth
 from Shape.Scene import Scene
 from Shape.SimpleHouse import RotatedRect
 import random
@@ -8,12 +8,18 @@ import os
 from PIL import Image
 
 image_outpath = "SampleImages/Image"
+ground_truth = "SampleImages/GroundTruth"
 ground_truth_outpath = "SampleImages/GroundTruth/training_solutions.json"
+
+test_outpath = "SampleImages/Test_Image"
+test_ground_truth_outpath = "SampleImages/Test_GroundTruth"
 
 final_json = []
 
-sample_size = 4
+sample_size = 10
 scene_size = (1024, 1024)
+
+test_size = 10
 
 for i in range(sample_size):
     curr_scene_id = "generated_img{0}".format(i)
@@ -21,6 +27,15 @@ for i in range(sample_size):
     max_buildings = 24
     fill_scene(max_buildings, scene, final_json)
     plot_sample_img(scene, image_outpath, i)
+    plot_ground_truth(scene, ground_truth, i)
+
+for i in range(test_size):
+    curr_scene_id = "test_img{0}".format(i)
+    scene = Scene(scene_size, curr_scene_id)
+    max_buildings = 24
+    fill_scene(max_buildings, scene, final_json)
+    plot_sample_img(scene, test_outpath, i)
+    plot_ground_truth(scene, test_ground_truth_outpath, i)
 
 with open(ground_truth_outpath, 'w') as outfile:
     json.dump(final_json, outfile)
